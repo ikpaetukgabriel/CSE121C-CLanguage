2608c2e651ee7e71c4d9ba12670e8ab6bd24c551
ikpaetukgabriel <gabrielikpaetuk@byui.edu>
2022-06-08 21:49:40 -0600

    begin task 1

--- week01/main.c
+++ week01/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+
 int main() {
-   printf("Hello, World!");
-   return 0;
+    char characterSample = 'G';
+    short shortSample = 4;
+    int integerSample = 2000;
+    long longSample = 123498765;
+
+    printf("Size of char: %zu, Value: %c bytes\n", sizeof(characterSample), characterSample);
+    printf("Size of : %zu, Value: %hi bytes\n", sizeof(shortSample), shortSample);
+    printf("Size int: %zu, Value: %d bytes\n", sizeof(integerSample), integerSample);
+    printf("Size long: %zu, Value: %ld bytes\n", sizeof(longSample), longSample);
+
+    float sampleFloat;
+    printf("Enter a floating point number: ");
+    scanf("%f", &sampleFloat);
+
+//    show as scientific notation
+    printf("You entered -> %e\n", sampleFloat);
+//    show as fixed point
+    printf("You entered -> %e\n", sampleFloat);
+    return 0;
 }
